3ddd41ddaf4ee516aff323061e7917338583aac3
Ilham Ellya Khahar <ilham.ellya@nawatech.co>
2025-07-09 18:30:14 +0700

    updated

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,5 +1,4 @@
 variable "vms" {
-  description = "List of VM definitions"
   type = list(object({
     vm_name     = string
     vm_pass     = string
@@ -7,11 +6,25 @@ variable "vms" {
     subnet      = string
     location    = string
     rg_name     = string
-    disk_name   = optional(string)
-    disk_size   = optional(number)
-    lun         = optional(number)
-    nsg_name    = optional(string)
-    sec_rules   = optional(list(object({
+
+    vm_user      = optional(string)
+    caching      = optional(string)
+    disk_sku     = optional(string)
+    os_publisher = optional(string)
+    os_offer     = optional(string)
+    os_sku       = optional(string)
+    os_version   = optional(string)
+    ephemeral    = optional(bool)
+    encryption   = optional(string)
+    lun          = optional(number)
+    public_ip    = optional(bool, true)
+    ssh_key      = optional(string)
+
+    disk_name    = optional(string)
+    disk_size    = optional(number)
+
+    nsg_name     = optional(string)
+    sec_rules    = optional(list(object({
       name         = string
       priority     = number
       direction    = string
@@ -22,7 +35,5 @@ variable "vms" {
       src_address  = string
       dest_address = string
     })), [])
-    public_ip   = optional(bool, true)
-    ssh_key     = optional(string)
   }))
 }
